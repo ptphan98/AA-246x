81d42d1d311ee315c26e9f0f6d521536066131d6
Philip Phan <ptphan98@gmail.com>
2021-04-21 16:40:55 -0700

    Revisions to code

--- Design_Space.m
+++ Design_Space.m
@@ -3,7 +3,7 @@ close all;
 clear all;
 % ALL UNITS IN SI %
 % Input Parameters to optimize:
-% W_L - Wing loading
+% W_L - Wing loading (Kg/m^2)
 % weight - aicraft weight (kg)
 % b - wing span (m)
 % V_cruise - Cruise velocity (m/s)
@@ -12,15 +12,28 @@ global rho
 rho = 1.225; %kg/m^3
 
 %attempt an optimization with constraints
+W_L_lo = 0;
+Weight_lo = 0;
+Span_lo = .0;
+V_cruise_lo = 0;
+
+Weight_up = 10;
+V_cruise_up = 50;
+
 x_0 = [5, 2, 2, 30];
 A = [-1 0 0 0;
-     1 0 0 0;
      0 -1 0 0;
      0 0 -1 0;
-     0 0 0  1];
-b = [0; 9; 0; 0; 50]; 
+     0 0 0 -1;%make sure inputs are greater than zero
+     0 1 0 0; 
+     0 0 0 1]; 
+b = [-W_L_lo; -Weight_lo; -Span_lo; -V_cruise_lo; Weight_up;V_cruise_up]; 
+Aeq = [];
+beq = [];
+lb = [];
+ub = [];
 
-x = fmincon( @(x) optimize( x(1),x(2),x(3),x(4)), x_0, A,b );
+x = fmincon( @(x) optimize( x(1),x(2),x(3),x(4)), x_0,A,b,Aeq,beq,lb,ub, @(x) nonl_const( x(1),x(2),x(3),x(4) ) );
 
 W_L_op = x(1)
 weight_op = x(2)
@@ -28,8 +41,11 @@ b_op = x(3)
 v_cruise_op = x(4)
 AR = b_op^2/(weight_op/W_L_op)
 
-[mass_total, mass_bat, mass_motor, mass_empty, L_D] = calc_weight(W_L_op, weight_op, b_op, v_cruise_op);
+%show optimized aircraft 
+[mass_total, mass_bat, mass_motor, mass_empty,T_W] = calc_weight(W_L_op, weight_op, b_op, v_cruise_op);
+[CL, Cd, Cdi , Cd0, L_D, v_ideal] = calc_aero(W_L_op, weight_op, b_op, v_cruise_op);
 [S_ref, c, s_htail, c_htail, s_vtail, c_vtail, l_t] = size_plane(W_L_op, weight_op, b_op, v_cruise_op);
+T_W
 battery_frac = mass_bat/mass_total
 motor_frac = mass_motor/mass_total
 structural_frac = mass_empty/mass_total
@@ -53,7 +69,6 @@ function [] = plot_aircraft(W_L, weight, b, v_cruise)
     
     figure;
     hold on
-    rectangle('Position',[.24*c 0-boom_dia/2 l_t boom_dia],'EdgeColor','k','LineWidth',1,'FaceColor',[.3 .3 .3]); %boom
     rectangle('Position',[(c/4 + l_t-.25*c_htail) (0-(s_htail/c_htail)/2) c_htail s_htail/c_htail],'EdgeColor','r','LineWidth',3); %horizontal tail
     rectangle('Position',[ (c/4 + l_t-.25*c_vtail) 0 c_vtail s_vtail/c_vtail],'EdgeColor','b','LineWidth',3); %vertical tail
     rectangle('Position',[0 0-b/2 c b],'EdgeColor','k','LineWidth',3); %wing
@@ -61,32 +76,43 @@ function [] = plot_aircraft(W_L, weight, b, v_cruise)
     axis equal
 end
 
-%%
+%% optimizing functions
+
 function score = optimize(W_L, weight, b, v_air)    
     %define useful parameters
     S_ref = weight./W_L;
     c = S_ref./b;
     AR = b.^2./S_ref;
     
-    [mass_total, mass_bat, mass_motor, mass_empty, L_D] = calc_weight(W_L, weight, b, v_air);
+    [mass_total, mass_bat, mass_motor, mass_empty] = calc_weight(W_L, weight, b, v_air);
+    [CL, Cd, Cdi , Cd0, L_D, v_ideal] = calc_aero(W_L, weight, b, v_air);
     
     %constraint - add constrains as necessary
     cost1 = abs(weight-mass_total); %make sure mass converges
-    cost2 = 10000;
-    if AR > 3.5
-        cost2  = 0; %force an aspect ratio
-    end
-    cost3 = 1000;
-    if L_D < 15
-        cost3 = 0; %set max limit for L/D
-    end
-   
-    
-    score = -v_air + cost1.*10 + cost2 + cost3; %maximize velocity given constraints
+    cost2 = abs(v_air - v_ideal); %make sure aircraft cruises at max L_D
+
+    score = -v_air; %maximize velocity given constraints
 end
 
+% nonlinear constraints
 
-%%
+function [c, ceq] = nonl_const(W_L, weight, b, v_air)
+    [mass_total, mass_bat, mass_motor, mass_empty] = calc_weight(W_L, weight, b, v_air);
+    [CL, Cd, Cdi , Cd0, L_D, v_ideal] = calc_aero(W_L, weight, b, v_air);
+    S_ref = weight./W_L;
+    c = S_ref./b;
+    AR = b.^2./S_ref;
+  
+    %constrain aspect ratio
+    AR_lower = 3.5;
+    AR_upper = 10;
+    %c = [- AR + AR_lower, AR - AR_upper];
+    c = [];
+    ceq = abs(weight-mass_total);
+end
+
+%% aircraft sizing functions
+
 function [S_ref, c, s_htail, c_htail, s_vtail, c_vtail, l_t] = size_plane(W_L, weight, b, v_air)
 S_ref = weight./W_L;
 %gives conventional tail dimensions based on wing geometry
@@ -109,13 +135,14 @@ s_vtail = vt_vol_cf.*S_ref.*b./l_t;
 c_vtail = sqrt(s_vtail./AR_vt);
 end 
 
-function [Cd,Cdi,Cd0] = calc_Cd(W_L, weight, b, v_air)
+function [CL, Cd, Cdi , Cd0, L_D, v_ideal] = calc_aero(W_L, weight, b, v_air)
 global rho
 [S_ref, c, s_htail, c_htail, s_vtail, c_vtail, l_t] = size_plane(W_L, weight, b, v_air);
 
 AR = b.^2./S_ref;
 CL = 9.81.*W_L./(0.5.*rho.*v_air.^2);
 e = .9; %this obviously varies with speed
+K = 1./(pi.*AR.*e);
 Cdi = CL.^2./(pi.*AR.*e); %lift induced drag
 
 %estimate fuselage area
@@ -132,8 +159,15 @@ Cdf_wing = calc_Cf(c,v_air).*(2.*S_ref)./S_ref;
 Cdf_tail = calc_Cf(c_htail,v_air).*(2*s_htail+2*s_vtail)./S_ref; 
 
 Cd0 = 1.25.*(Cdf_nose + Cdf_fuse + Cdf_wing + Cdf_tail);
-
 Cd = Cd0 + Cdi;
+
+L_D = CL/Cd;
+
+%Find the optimum cruise speed, v_ideal, that maximizes L/D for this
+%configuration
+Cl_opt = sqrt(Cd0/K);
+v_ideal = sqrt( (2*9.81*weight)/(rho*Cl_opt*S_ref) );
+
 
     function CF = calc_Cf(l,v_air)
         %flat plate assumption for Cf
@@ -148,13 +182,8 @@ Cd = Cd0 + Cdi;
     end
 end
 
-function [mass_bat, L_D] = battery_weight(W_L, weight, b, v_air)
-    S_ref = weight./W_L;
-    [Cd,Cdi,Cd0] = calc_Cd(W_L, weight, b, v_air);
-    AR = b.^2./S_ref;
-    e = .99;
-    K = 1./(pi.*AR.*e);
-    L_D = 1./(2.*sqrt(Cd0.*K));
+function [mass_bat] = battery_weight(W_L, weight, b, v_air)
+    [CL, Cd, Cdi , Cd0, L_D, v_ideal] = calc_aero(W_L, weight, b, v_air);
    
     eta_sys = .374; %battery + propellor + motor effiency estimate
     range = 42195/2; %meters
@@ -162,20 +191,20 @@ function [mass_bat, L_D] = battery_weight(W_L, weight, b, v_air)
     g = 9.807; %m/s^2
   
     mass_bat = range.*g.*weight./(L_D.*eta_sys.*H_batt);
-    
 end
 
-function [mass_motor] = motor_weight(W_L, weight, b, v_air)
+function [mass_motor,T_W] = motor_weight(W_L, weight, b, v_air)
     global rho
     S_ref = weight./W_L;
     
-    %calculates thrust required for cruise at max L/D
-    [Cd,Cdi,Cd0] = calc_Cd(W_L, weight, b, v_air);
-    Power_cruise = 1/2.*rho.*v_air.^3.*S_ref.*(2.*Cd0); %watts - At max L/D, Cd0 = Cdi
+    %calculates thrust required for cruise at v_air
+    [CL, Cd, Cdi , Cd0, L_D, v_ideal] = calc_aero(W_L, weight, b, v_air);
+    Thrust = 1/2.*rho.*v_air.^2.*S_ref.*(Cd); %Newtons
+    Power_cruise = 1/2.*rho.*v_air.^3.*S_ref.*(Cd); %watts
     Power_max = Power_cruise./0.40; %assume motor runs most efficient at 40% max power
     
     motor_p_density = 5.101/0.001; %watts/kg of a motor 
-    
+    T_W = Power_max/v_air/(weight*9.81); %ballpark thrust to weight
     mass_motor =  1./motor_p_density.*Power_max;
 end
 
@@ -191,14 +220,14 @@ function [mass_empty] = empty_weight(W_L, weight, b, v_air)
     m_wing = S_ref.*1.31346914; %kg/m^2 x s_wing
     m_htail = s_htail.*1.695180676; %kg/m^2 x s_htail
     m_vtail = s_vtail.*0.792369773; %kg/m^2 x s_vtail 
-    m_fuse = (pi.*r_fus.^2).*l_fus.*29.43741259; %kg/m^3 x vol_fuselage 
+    m_fuse = (pi.*r_fus.^2).*l_fus.*29.43741259; %kg/m^2 x s_fuselage 
     
     mass_empty = m_wing + m_htail + m_vtail + m_fuse;
 end
 
-function [mass_total, mass_bat, mass_motor, mass_empty, L_D] = calc_weight(W_L, weight, b, v_air)
-    [mass_bat, L_D] = battery_weight(W_L, weight, b, v_air);
-    [mass_motor] = motor_weight(W_L, weight, b, v_air);
+function [mass_total, mass_bat, mass_motor, mass_empty, T_W] = calc_weight(W_L, weight, b, v_air)
+    [mass_bat] = battery_weight(W_L, weight, b, v_air);
+    [mass_motor,T_W] = motor_weight(W_L, weight, b, v_air);
     [mass_empty] = empty_weight(W_L, weight, b, v_air);
     
     mass_total = mass_bat + mass_motor + mass_empty;
